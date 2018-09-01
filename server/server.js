67d0911245eb785fb140fd25afdbcded9c8ca540
tzetzo <tzvetanmarinov@yahoo.com>
2018-09-01 22:29:05 +0300

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express'); //import express
 const path = require('path'); //import path module
-
-const port = process.env.PORT || 3000; //declare the port; process.env.PORT is provided by heroku
-
-const app = express(); //create express application
+const http = require('http'); //needed for socket.io
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-app.use(express.static(publicPath));
+const port = process.env.PORT || 3000; //declare the port; process.env.PORT is provided by heroku
+const app = express(); //create express application
+const server = http.createServer(app);  //server needed for socket.io
+const io = socketIO(server);  //the websockets server
 
-app.listen(port, () => {
+app.use(express.static(publicPath));  //the static web site
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
