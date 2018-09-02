34965dd94b866647dbdee19faaacec8a9064086b
tzetzo <tzvetanmarinov@yahoo.com>
2018-09-02 09:43:07 +0300

    added newMessage & createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+const socket = io();
+
+socket.on('connect', function () {
+    console.log('connected to server');
+
+    socket.emit('createMessage', {
+      to: 'lili@example.com',
+      text: 'hey this is tzetzo'
+    });
+
+});
+
+socket.on('disconnect', function () {
+    console.log('disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage: ', message);
+});
--- server/server.js
+++ server/server.js
@@ -11,11 +11,21 @@ const io = socketIO(server);  //the websockets server
 
 app.use(express.static(publicPath));  //the static web site
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => { //the only time we use io.on
     console.log('new user connected');
 
     socket.on('disconnect', () => {
         console.log('client disconnected');
+    });
+
+    socket.emit('newMessage', {
+        from: 'lili',
+        text: 'hey what is going on tzetzo.',
+        createdAt: 1234567
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
     });
 });
 
